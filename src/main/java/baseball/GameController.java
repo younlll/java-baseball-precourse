09c1e92e0e999add2161b8ee30a86b7d91351678
이유나 <dragonyuna@naver.com>
2021-10-04 20:47:31 +0900

    유저 입력에 대한 스트라이크 및 볼 카운트 기능 추가

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -30,6 +30,63 @@ public class GameController {
     }
 
     /**
+     * 게임 중간 집계
+     * @param computerNum
+     * @param userNum
+     * @return 게임종료 여부 반환(true = 종료O, false = 종료X)
+     */
+    public static Boolean isGameMidwayCount(String computerNum, String userNum){
+        isCheckedStrikeBall(computerNum, userNum);
+        if(!baseballScore.isNothing()){
+            printStrikeBall();
+        }
+
+        return isCheckedFinish();
+    }
+
+    /**
+     * 스트라이크 3개로 유저가 컴퓨터의 숫자를 맞췄는지 확인
+     * @return 게임종료 여부 반환(true = 종료O, false = 종료X)
+     */
+    private static Boolean isCheckedFinish(){
+        if(baseballScore.getStrike() == Constant.NUMBER_MAX_LENGTH){
+            System.out.println(Constant.GAME_FINISH_MSG);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 스트라이크와 볼 카운트 결과 출력
+     */
+    private static void printStrikeBall(){
+        if(baseballScore.getStrike() > 0){
+            System.out.print(baseballScore.getStrike() + "스트라이크 ");
+        }
+        if(baseballScore.getBall() > 0){
+            System.out.print(baseballScore.getBall() + "볼");
+        }
+        System.out.println();
+    }
+
+    /**
+     * 유저의 입력한 값을 컴퓨터 숫자와 비교하여 스트라이크 및 볼 카운트
+     * @param computerNum
+     * @param userNum
+     */
+    private static void isCheckedStrikeBall(String computerNum, String userNum){
+        for(int ii = 0; ii < Constant.NUMBER_MAX_LENGTH; ii++){
+            if(computerNum.charAt(ii) == userNum.charAt(ii)){
+                baseballScore.setStrike(baseballScore.getStrike() + 1);
+            }
+            if(computerNum.charAt(ii) != userNum.charAt(ii)
+                    && computerNum.indexOf(userNum.charAt(ii)) > -1){
+                baseballScore.setBall(baseballScore.getBall() + 1);
+            }
+        }
+    }
+
+    /**
      * 입력값 범위에 대한 유효성 검사
      * 유저의 입력값 각 숫자 범위가 1~9사이 인지 확인
      * @param userNum
